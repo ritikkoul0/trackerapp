263025e40c6243221240e1a594c945a659543331
ritikkoul <61817094+ritikkoul0@users.noreply.github.com>
2025-12-27 12:19:42 +0530

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 const config = {
-  API_URL: process.env.REACT_APP_API_URL || 'https://trackerbackend-ao16.onrender.com'
+  API_URL: 'https://trackerbackend-ao16.onrender.com'
 };
 
 export default config;
